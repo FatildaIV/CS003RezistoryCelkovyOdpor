75fc339070ccd3d09e12aa8a209d541aa74e1e4c
Fatilda IV <30930670+FatildaIV@users.noreply.github.com>
2021-06-11 12:34:45 +0200

    Initial commit

---
+++ CS003RezistoryCelkovyOdpor/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CS003RezistoryCelkovyOdpor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(P(S(5, P(10, 10)), 10));
+            Console.ReadKey();
+        }
+        static float S(float a, float b)
+        {
+            return a + b;
+        }
+        static float P(float a, float b)
+        {
+            return 1 / (1 / a + 1 / b);
+        }
+    }
+}
